f2c744e1ed1bbd9f3544ea5af461b58e91682a2f
inandexhale95 <inandexhale95@gmail.com>
2022-04-24 03:20:30 +0900

    Add project files.

---
+++ Introduce/Controllers/ForumController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Introduce.Controllers
+{
+    public class ForumController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Introduce/Controllers/FreeBoardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Introduce.Controllers
+{
+    public class FreeBoardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
