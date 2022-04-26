6aca45bcdc46b63bd008304984f3663082f5bc6c
inandexhale95 <inandexhale95@gmail.com>
2022-04-26 10:16:46 +0900

    Update FreeBoard DateTime, Pagination

---
+++ Introduce.Data/ViewModels/FreeBoardPagination.cs
@@ -0,0 +1,16 @@
+﻿using Introduce.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Introduce.Data.ViewModels
+{
+    public class FreeBoardPagination
+    {
+        public int CurrentPageIndex { get; set; }
+        public int PageCount { get; set; }
+        public IEnumerable<FreeBoard> FreeBoards { get; set; }
+    }
+}
--- Introduce/Controllers/FreeBoardController.cs
+++ Introduce/Controllers/FreeBoardController.cs
@@ -1,25 +1,62 @@
 ﻿using Introduce.Data.Models;
 using Introduce.Data.ViewModels;
+using Introduce.Services.Contexts;
 using Introduce.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Introduce.Controllers
 {
     public class FreeBoardController : Controller
     {
         private readonly IFreeBoard _freeBoard;
+        private AppDbContext _context;
+        private FreeBoardPagination _pagination;
 
-        public FreeBoardController(IFreeBoard freeBoard)
+        public FreeBoardController(IFreeBoard freeBoard,
+                                   AppDbContext context)
         {
             _freeBoard = freeBoard;
+            _context = context;
+            _pagination = new FreeBoardPagination();
         }
 
-        [HttpGet]
-        public IActionResult Index()
-        {
-            var freeBoardList = _freeBoard.GetFreeBoardList();
+        #region privates method
 
-            return View(freeBoardList);
+        private async Task<FreeBoardPagination> GetPaginationAsync(int currentPage = 1)
+        {
+            const int maxRowsPerPage = 10;
+
+            _pagination.FreeBoards = await _context.FreeBoards
+                .OrderByDescending(x => x.FreeBoardSeq)
+                .Skip((currentPage - 1) * maxRowsPerPage)
+                .Take(maxRowsPerPage)
+                .ToListAsync();
+
+            double pageCount = (double)((decimal)_context.FreeBoards.Count() / Convert.ToDecimal(maxRowsPerPage));
+
+            _pagination.PageCount = (int)Math.Ceiling(pageCount);
+            _pagination.CurrentPageIndex = currentPage;
+            return _pagination;
+        }
+
+        #endregion
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var model = await GetPaginationAsync();
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Index(int currentPage)
+        {
+            var model = await GetPaginationAsync(currentPage);
+
+            return View(model);
         }
 
         [HttpPost]
